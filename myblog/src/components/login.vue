ac6dd2197d19e137da88f14f3f352eba0c154f8f
Hiphopweb <894744332@qq.com>
2018-07-01 22:37:15 +0800

    login

--- myblog/src/components/login.vue
+++ myblog/src/components/login.vue
@@ -1,18 +1,60 @@
 <template>
-  <div>
-    class
+  <div id="main" :style="note" style="position:absolute">
+    <div class="login">
+
+      <el-form ref="form" :model="form" label-width="80px">
+        <el-form-item label="用户名">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+
+        <el-form-item label="密码">
+          <el-input v-model="form.psw"></el-input>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">立即创建</el-button>
+          <el-button>取消</el-button>
+        </el-form-item>
+      </el-form>
+
+    </div>
   </div>
+
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'login',
   data() {
-    return {}
+    return {
+      note: {
+        backgroundImage: 'url(' + require('./../../static/img/bgimg.jpg') + ')',
+        backgroundSize: '100% 100%'
+      },
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      }
+    }
   }
 }
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#main {
+  height: 100%;
+  width: 100%;
+  /* position: absolute; */
+}
+.login {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 </style>
